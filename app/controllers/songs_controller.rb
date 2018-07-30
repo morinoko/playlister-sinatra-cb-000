f23ad24312dce456db3e347d8ddc3647a1d2b0e6
Felice Forby <feliceforby@gmail.com>
2018-07-30 23:36:47 +0000

    build new song form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,30 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    
+    @song = Song.create(params[:song])
+
+    if params[:artist][:name]
+      artist_name = params[:artist][:name]
+
+      @artist = Artist.find_by(name: artist_name) || Artist.create(name: artist_name)
+      @song.artist = @artist
+    end
+
+    if params[:genre][:name]
+      genre_name = params[:genre][:name].downcase
+
+      @genre = Genre.find_by(name: genre_name) || Genre.create(name: genre_name)
+      @song.genres << @genre
+    end
+
+    @song.save
+
+    redirect to "songs/#{@song.slug}"
   end
 
   get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
     erb :'songs/new'
   end
 
